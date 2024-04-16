65aadb6ecb9a973b1d8faa0215fd36834083f55f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-16 03:07:31 -0300

    feat: create function to list all tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,30 @@
+import { ITask } from "../interfaces/Task";
 import styles from "./TaskList.module.css";
 
-type Props = {};
+type Props = {
+  taskList: ITask[];
+};
 
-const TaskList = (props: Props) => {
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>TaskList</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
   );
 };
 
